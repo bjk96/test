683dc8e8ea52c84fc1cc505a2bd2151eecc66677
bjk96 <bjk5371@gmail.com>
2021-03-05 14:47:21 +0900

    주석 추가

--- src/spms/annotation/Component.java
+++ src/spms/annotation/Component.java
@@ -3,6 +3,7 @@ package spms.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+// modify
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Component {
   String value() default "";
